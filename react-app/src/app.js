1370044f929108796163517f1ab290f512904ff0
Laxmanapandi Baskaran <laxmanapandi.baskaran@cdw.com>
2023-05-23 09:22:04 +0530

    React app without create-react-app

---
+++ react-app/src/app.js
@@ -0,0 +1,13 @@
+import React from "react";
+import reactDom from "react-dom";
+
+const App = () =>{
+   
+    return (
+        <h1>
+            Welcome to React App thats build using Webpack and Babel separately
+        </h1>
+    )
+}
+
+export default App
